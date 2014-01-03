0ed67ec48cbe6a93ad6caf48e2f32d1be4c31d0f
Finn Ellis <finnre@cat.pdx.edu>
2014-01-03 00:42:17 -0800

    Base test cleanup and an extra deal test.

--- tests/test_base.py
+++ tests/test_base.py
@@ -8,30 +8,36 @@ from pydeck import base
 
 class TestBase(unittest.TestCase):
     def test_property_attrs(self):
-        foo = base.CardProperty("bar")
-        self.assertEqual(foo.name, "bar")
-        self.assertEqual(foo.plural, "bars")
-        self.assertEqual(foo.short, "b")
+        prop = base.CardProperty("bar")
+        self.assertEqual(prop.name, "bar")
+        self.assertEqual(prop.plural, "bars")
+        self.assertEqual(prop.short, "b")
 
     def test_shuffle(self):
         data = [1, 2, 3, 4, 5]
-        foo = base.Hand(data)
-        self.assertEqual(foo.data, data)
+        hand = base.Hand(data)
+        self.assertEqual(hand.data, data)
         random.seed(0)
-        foo.shuffle()
-        self.assertNotEqual(foo.data, data)
+        hand.shuffle()
+        self.assertNotEqual(hand.data, data)
 
     def test_deal(self):
-        foo = base.Hand([1, 2, 3, 4 ,5])
-        bar = foo.deal(3)
-        self.assertEqual(foo.data, [4, 5])
-        self.assertEqual(bar.data, [1, 2, 3])
+        hand = base.Hand([1, 2, 3, 4, 5])
+        book = hand.deal(3)
+        self.assertEqual(hand.data, [4, 5])
+        self.assertEqual(book.data, [1, 2, 3])
+
+    def test_deal_toomany(self):
+        hand = base.Hand([1, 2, 3, 4, 5])
+        self.assertRaises(IndexError, hand.deal, 10)
 
 
 class TestEqualityMixin(unittest.TestCase):
     def setUp(self):
-        class Foo(base.EqualityMixin): pass
-        class Bar(base.EqualityMixin): pass
+        class Foo(base.EqualityMixin):
+            pass
+        class Bar(base.EqualityMixin):
+            pass
         self.Foo = Foo
         self.Bar = Bar
 
